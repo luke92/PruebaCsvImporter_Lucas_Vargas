a9082608c9e91cf9a500175e693c907e00cd7c54
Lucas Vargas <lucasjv92@gmail.com>
2021-09-30 20:29:27 -0300

    First commit

---
+++ CsvImporter/CsvImporter.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CsvImporter.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
